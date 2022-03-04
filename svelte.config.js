9bc69f2f7c69cde1b7dde02ac1fc39b79f1d5231
yomu85 <yomu85@gmail.com>
2022-03-04 19:06:59 +0900

    package, svelte config 수정

--- svelte.config.js
+++ svelte.config.js
@@ -2,29 +2,10 @@ import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import autoprefixer from 'autoprefixer';
 
-const production = process.env.NODE_ENV === 'production'
-
-function babelOptions() {
-  return {
-    plugins: production 
-    ? ['transform-remove-console'] 
-    : []
-  }
-}
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-    adapter: adapter(),
-    outDir: '.svelte-kit',
-    prerender: {
-      concurrency: 1,
-      crawl: true,
-      enabled: true,
-      entries: ['*'],
-      onError: 'fail'
-    },
-		vite: () => ({})
+    adapter: adapter()
   },
 	preprocess: preprocess({
 		postcss: {
@@ -34,8 +15,7 @@ const config = {
 		},
 		scss: {
 			prependData: `@import './src/scss/main.scss';`,
-		},
-		babel: babelOptions()
+		}
 	})
 };
 
